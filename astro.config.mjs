66598fa40cb32a21807233cbaf1038c692f21756
Kamil M <116840531+proxima812@users.noreply.github.com>
2024-11-12 18:57:26 +0500

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://12-traditions.github.io",
+	base: "12-traditions",
 	integrations: [tailwind()],
 })
